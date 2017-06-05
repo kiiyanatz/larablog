55f22fec61c3c65fa6ed9575f47d5f5555edf162
kiiyanatz <kiiyanatz@gmail.com>
2017-06-05 12:06:26 +0300

    New styles

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -10,7 +10,7 @@ class BlogController extends Controller
 
 	public function getIndex()
     {
-    	$posts = Post::orderBy('id', 'desc')->paginate(5);
+    	$posts = Post::orderBy('id', 'desc')->paginate(10);
     	return view('blog.index')->with('posts', $posts);
     }
 
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -3,17 +3,38 @@
 | All Posts
 @endsection
 @section('stylesheets')
-    <style>
+{{--     <style>
       #left {
-        text-align: center;
+        position: relative;
         font-size: 20px;
       }
 
-      img {
-        border-radius: 10px;
+      .post-title {
+        color: black;
+        font-family: monospace;
       }
 
-    </style>
+      .post-detail {
+        font-size: 10px;
+        color: #ccc;
+      }
+
+      .post-brief {
+        color: #aaa;
+        font-size: 15px;
+        font-family: monospace;
+      }
+
+      #more {
+        color: black;
+        text-transform: uppercase;
+        font-size: .65em;
+        font-weight: 700;
+        text-decoration: underline;
+      }
+
+    </style> --}}
+    <link rel="stylesheet" href="/css/home.css">
 @stop
 @section('content')
 <div class="container">
@@ -22,21 +43,35 @@
             <h1 class="text-center">Blog Archive</h1>
         </div>
     </div>
-    @foreach($posts as $post)
+{{--     @foreach($posts as $post)
       <hr>
         <div class="row" id="post-this">
           <div class="col-md-5" id="right">
             <img src="{!! asset('imgs/uploads/' . $post->image) !!}" alt="" width="100%">
           </div>
             <div class="col-md-7" id="left">
-                <h2>{!! $post->title !!}</h2>
-                <h5><b><i>Published: {{ date('M j, Y h:i a', strtotime($post->created_at)) }}</i></b></h5>
-                {!! strip_tags(substr($post->body, 0, 250)) !!}
-                {!! strip_tags(strlen($post->body)) > 250 ? '...' : '' !!} <br />
-                <a href="blog/{{ $post->slug }}">Read More</a>
+                <h2 class="post-title">{!! $post->title !!}</h2>
+                <h5 class="post-detail"><b>Published: {{ date('M j, Y h:i a', strtotime($post->created_at)) }}</b></h5>
+                <p class="post-brief">
+                    {!! strip_tags(substr($post->body, 0, 250)) !!}
+                {!! strip_tags(strlen($post->body)) > 250 ? '...' : '' !!}
+                </p>
+                <a href="blog/{{ $post->slug }}" id="more">Read More</a>
             </div>
         </div>
-    @endforeach
+    @endforeach --}}
+    <div class="row">
+        @foreach($posts as $post)
+        <div id="latest-blog" class="col-md-3">
+            <div class="post">
+                <h3 class="post-title">{!! $post->title !!}</h3>
+                <img src="{!! asset('imgs/uploads/' . $post->image) !!}" alt="">
+                <p class="post-desc">{!! substr(strip_tags($post->body), 0, 300) !!} {!! strlen(strip_tags($post->body)) > 300 ? "..." : ""!!}</p>
+                <a href="blog/{{ $post->slug }}" class="more">Read more</a>
+            </div>
+        </div>
+        @endforeach
+    </div>
 
     <div class="row">
         <div class="col-md-12">
